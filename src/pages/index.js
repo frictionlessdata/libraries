131bd26c56e3c390a8bb838aa88691255b02c230
roll <eskarev@gmail.com>
2021-08-18 18:12:14 +0300

    Updated the hero text

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,8 +57,8 @@ function Home() {
       </div>
       <div className={classnames(styles.announcement, styles.announcementDark)}>
         <div className={styles.announcementInner}>
-          Frictionless is a set of libraries implementing Frictionless
-          Specifications for 10 programming languages.
+          Frictionless is a set of libraries implementing Frictionless Standards
+          for 10 programming languages.
         </div>
       </div>
       <div className={styles.section}>
