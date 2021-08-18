d18f407e9458c071014f90c681c64eede3b05194
roll <eskarev@gmail.com>
2021-08-18 16:16:21 +0300

    Automated tableschema-jl

--- docs/build.js
+++ docs/build.js
@@ -12,7 +12,8 @@ const DOCS = [
   // { repo: "tableschema-php", path: "table-schema/php.md", branch: "master" },
   // { repo: "tableschema-java", path: "table-schema/java.md" },
   // { repo: "tableschema-go", path: "table-schema/go.md" },
-  { repo: "tableschema-r", path: "table-schema/rlang.md", branch: "master" },
+  // { repo: "tableschema-r", path: "table-schema/rlang.md", branch: "master" },
+  { repo: "TableSchema.jl", path: "table-schema/julia.md" },
 ];
 
 // Main
@@ -20,11 +21,12 @@ const DOCS = [
 async function main() {
   for (const doc of DOCS) {
     const url = `${URL}/${doc.repo}/${doc.branch || "main"}/README.md`;
+    const name = doc.repo.replace(".", "-").toLowerCase();
     const path = `docs/${doc.path}`;
     const res = await fetch(url);
     if (!res.ok) throw `Network error: ${url}`;
     let text = await res.text();
-    text = text.replace(/^# (.*)/, `---\ntitle: ${doc.repo}\n---`);
+    text = text.replace(/^# (.*)/, `---\ntitle: ${name}\n---`);
     fs.writeFileSync(path, text);
     console.log(`Built: ${path}`);
   }
